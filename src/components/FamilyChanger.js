9452f77d17f901db1c943ecfe6ce2a54b1bfb71d
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-11 13:00:06 -0600

    adds updated react-2 mini project

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export default function FamilyChanger() {
+export default function FamilyChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select       
+    onChange={e => props.update(e.target.value)} 
+    className="dropDownContainer"
+    disabled={props.allowEdit === 'false' }
+    >
       <option value="monospace"> Monospace </option>
       <option value="arial"> Arial </option>
       <option value="courier"> Courier </option>
